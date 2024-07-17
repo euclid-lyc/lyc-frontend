71fa147d31cfc2d874412cc193bf0864d83b469b
dodo <108921606+doyeon0307@users.noreply.github.com>
2024-07-17 17:59:35 +0900

    Revert "Revert "[feat] follow list, post screen, appbar""

---
+++ lyc_flutter_project/lib/data/follow_data.dart
@@ -0,0 +1,18 @@
+import 'package:lyc_flutter_project/model/profile.dart';
+
+class FollowData {
+  static List<Profile> follower = [
+    Profile('ex_profile', 'Winter', '@imwinter', '상태메시지'),
+    Profile('ex_profile', 'Winter', '@imwinter', '상태메시지'),
+    Profile('ex_profile', 'Winter', '@imwinter', '상태메시지'),
+    Profile('ex_profile', 'Winter', '@imwinter', '상태메시지'),
+  ];
+  static List<Profile> follwing = [
+    Profile('ex_profile', 'Alice', '@alice', '상태메시지'),
+    Profile('ex_profile', 'Alice', '@alice', '상태메시지'),
+    Profile('ex_profile', 'Alice', '@alice', '상태메시지'),
+    Profile('ex_profile', 'Alice', '@alice', '상태메시지'),
+    Profile('ex_profile', 'Alice', '@alice', '상태메시지'),
+    Profile('ex_profile', 'Alice', '@alice', '상태메시지'),
+  ];
+}
---
+++ lyc_flutter_project/lib/model/profile.dart
@@ -0,0 +1,8 @@
+class Profile {
+  final String photo;
+  final String name;
+  final String id;
+  final String msg;
+
+  Profile(this.photo, this.name, this.id, this.msg);
+}
