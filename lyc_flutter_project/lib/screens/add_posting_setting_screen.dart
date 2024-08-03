8ba696ccad37ebba64e2f747206ec6b68b4ed37f
dodo <eg4rang@naver.com>
2024-08-03 12:12:28 +0900

    [feat] add link

--- lyc_flutter_project/lib/screens/add_posting_setting_screen.dart
+++ lyc_flutter_project/lib/screens/add_posting_setting_screen.dart
@@ -29,6 +29,8 @@ class _AddPostingSettingScreenState extends State<AddPostingSettingScreen> {
   int minTempValue = 3;
   int maxTempValue = 10;
 
+  final GlobalKey _imageKey = GlobalKey();
+
   final List<String> styleButtons = [
     '클래식',
     '캐주얼',
@@ -97,70 +99,79 @@ class _AddPostingSettingScreenState extends State<AddPostingSettingScreen> {
             const SizedBox(height: 15),
             GestureDetector(
               onTapDown: (details) {
-                _addPoint(details.localPosition);
+                final RenderBox renderBox =
+                    _imageKey.currentContext?.findRenderObject() as RenderBox;
+                final localOffset =
+                    renderBox.globalToLocal(details.globalPosition);
+                _addPoint(localOffset); // Pass Offset directly
               },
-              child: Stack(children: [
-                AspectRatio(
-                  aspectRatio: (3/4),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(20),
-                    child: Image.file(
-                      File(widget.image.path),
-                      fit: BoxFit.cover,
+              child: Stack(
+                children: [
+                  AspectRatio(
+                    aspectRatio: (3 / 4),
+                    child: ClipRRect(
+                      key: _imageKey,
+                      borderRadius: BorderRadius.circular(20),
+                      child: Image.file(
+                        File(widget.image.path),
+                        fit: BoxFit.cover,
+                      ),
                     ),
                   ),
-                ),
-                ..._points.map(
-                      (e) {
-                    Offset offset = e["offset"];
-                    double left = offset.dx;
-                    double top = offset.dy;
+                  ..._points.map(
+                    (e) {
+                      Offset offset = e["offset"];
+                      final RenderBox renderBox = _imageKey.currentContext
+                          ?.findRenderObject() as RenderBox;
+                      final size = renderBox.size;
 
-                    // Define the size of the container
-                    const double containerWidth = 60; // Adjust the size as needed
-                    const double containerHeight = 30; // Adjust the size as needed
+                      double left = offset.dx * size.width;
+                      double top = offset.dy * size.height;
 
-                    // Adjust position to ensure the container stays within bounds
-                    if (left + containerWidth >= MediaQuery.of(context).size.width - 30) {
-                      left = left - containerWidth;
-                    }
-                    if (top + containerHeight >= MediaQuery.of(context).size.height - 30) {
-                      top = top - containerHeight;
-                    }
-                    if (left < 0) {
-                      left = 0;
-                    }
-                    if (top < 0) {
-                      top = 0;
-                    }
+                      const double containerWidth = 60;
+                      const double containerHeight = 30;
 
-                    return Positioned(
-                      left: left,
-                      top: top,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Color(0xff252525),
-                        ),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          '링크${_points.indexOf(e) + 1}',
-                          style: const TextStyle(
-                            color: Colors.white,
+                      if (left + containerWidth > size.width) {
+                        left = size.width - containerWidth;
+                      }
+                      if (top + containerHeight > size.height) {
+                        top = size.height - containerHeight - 20;
+                      }
+                      if (left < 0) {
+                        left = 0;
+                      }
+                      if (top < 0) {
+                        top = 0;
+                      }
+
+                      return Positioned(
+                        left: left,
+                        top: top,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: const Color(0xff252525),
+                          ),
+                          padding: const EdgeInsets.all(10),
+                          child: Text(
+                            '링크${_points.indexOf(e) + 1}',
+                            style: const TextStyle(
+                              color: Colors.white,
+                            ),
                           ),
                         ),
-                      ),
-                    );
-                  },
-                ).toList(),
-              ]),
+                      );
+                    },
+                  ).toList(),
+                ],
+              ),
             ),
-            const SizedBox(height: 20),
+            const SizedBox(height: 10),
             ..._points.map(
               (e) {
                 return Container(
-                  padding: EdgeInsets.only(right: 10),
-                  margin: EdgeInsets.symmetric(horizontal: 10),
+                  margin: const EdgeInsets.symmetric(vertical: 8),
+                  padding: const EdgeInsets.only(right: 10),
                   height: 40,
                   width: double.infinity,
                   decoration: BoxDecoration(
@@ -190,7 +201,12 @@ class _AddPostingSettingScreenState extends State<AddPostingSettingScreen> {
                               ),
                             ),
                             const SizedBox(width: 10),
-                            Expanded(child: Text(e['link'], overflow: TextOverflow.ellipsis,)),
+                            Expanded(
+                              child: Text(
+                                e['link'],
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
                           ],
                         ),
                       ),
@@ -311,22 +327,36 @@ class _AddPostingSettingScreenState extends State<AddPostingSettingScreen> {
     );
   }
 
-  void _addPoint(Offset offset) {
+  void _addPoint(Offset localOffset) {
+    final RenderBox renderBox =
+        _imageKey.currentContext?.findRenderObject() as RenderBox;
+    final size = renderBox.size;
+
+    double relativeDx = localOffset.dx / size.width;
+    double relativeDy = localOffset.dy / size.height;
+
     TextEditingController _linkController = TextEditingController();
     showDialog(
       context: context,
       builder: (context) => AlertDialog(
-        title: Text("Enter Link"),
+        title: const Text('링크를 입력해주세요'),
         content: TextField(controller: _linkController),
         actions: [
           TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: const Text('취소'),
+          ),
+          TextButton(
             onPressed: () {
               setState(() {
-                _points.add({"offset": offset, "link": _linkController.text});
+                _points.add({
+                  "offset": Offset(relativeDx, relativeDy),
+                  "link": _linkController.text
+                });
               });
               Navigator.of(context).pop();
             },
-            child: Text("Add"),
+            child: const Text('추가하기'),
           ),
         ],
       ),
