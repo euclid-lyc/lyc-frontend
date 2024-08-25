204b3d4408a170e19634a2f6bfc7020055566975
doyeon0307 <eg4rang@naver.com>
2024-08-26 01:49:07 +0900

    [refactor] mypage

--- lyc_flutter_project/lib/main.dart
+++ lyc_flutter_project/lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:lyc_flutter_project/common/screen/splash_screen.dart';
 import 'package:lyc_flutter_project/Join/Screens/join_membership_screen_4.dart';
+import 'package:lyc_flutter_project/mypage/provider/block_provider.dart';
+import 'package:lyc_flutter_project/mypage/provider/follow_provider.dart';
+import 'package:lyc_flutter_project/mypage/provider/notify_provider.dart';
 import 'package:provider/provider.dart';
 
 void main() {
   runApp(
     MultiProvider(providers: [
       ChangeNotifierProvider(create: (context) => MembershipState()),
+      ChangeNotifierProvider(create: (context) => BlockProvider(),),
+      ChangeNotifierProvider(create: (context) => NotifyProvider(),),
+      ChangeNotifierProvider(create: (context) => FollowProvider(),),
       // 다른 프로바이더들도 여기에 추가
-    ], child: MyApp()),
+    ], child: const MyApp()),
   );
 }
 
