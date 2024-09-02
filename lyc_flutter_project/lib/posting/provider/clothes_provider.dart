c477fa6e960b73f0775530b809b72b8f360a6963
doyeon0307 <eg4rang@naver.com>
2024-09-02 11:12:05 +0900

    [del] 필요없는 코드 삭제

--- lyc_flutter_project/lib/posting/provider/clothes_provider.dart
+++ lyc_flutter_project/lib/posting/provider/clothes_provider.dart
@@ -1,10 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
-
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:lyc_flutter_project/common/utils/data_utils.dart';
 import 'package:lyc_flutter_project/data/temp_member_data.dart';
 import 'package:lyc_flutter_project/posting/model/clothes_posting.dart';
 import 'package:lyc_flutter_project/posting/repository/clothes_repository.dart';
@@ -20,41 +17,26 @@ class ClothesProvider extends ChangeNotifier {
   });
 
   void updateImage(XFile image) {
-    // _postingImage = _postingImage.copyWith(image: image.path);
-    // final file = await DataUtils.pathToMultipart(_postingImage.image!);
-    // _postingImage.copyWith(image: image.path);
     _postingImage = ClothesPostingImage(
       memberId: _postingImage.memberId,
       image: image.path,
       title: _postingImage.title,
       text: _postingImage.text,
     );
-    print("path: ${_postingImage.image}");
-    // print("update image: $text");
     notifyListeners();
   }
 
   void updateTitle(String text) {
     _postingImage = _postingImage.copyWith(title: text);
-    // print("update title: $text");
     notifyListeners();
   }
 
   void updateContent(String text) {
     _postingImage = _postingImage.copyWith(text: text);
-    // print("update content: $text");
     notifyListeners();
   }
 
   Future<bool> canUploadImage() async {
-    // if (_postingImage.memberId != null &&
-    //     _postingImage.image != null &&
-    //     _postingImage.text != null &&
-    //     _postingImage.title != null) {
-    //   return true;
-    // } else {
-    //   return false;
-    // }
     if (_postingImage.memberId == null) {
       print("memberId is null");
       return false;
@@ -76,7 +58,6 @@ class ClothesProvider extends ChangeNotifier {
 
   void uploadImage() async {
     if (await canUploadImage()) {
-      // final file = await DataUtils.pathToMultipart(_postingImage.image!);
       final clothesByImageDTO = jsonEncode({
         'memberId': _postingImage.memberId!,
         'title': _postingImage.title!,
