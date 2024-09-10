020ff172223c8e2ffdfad3d958932675587f5db3
doyeon0307 <eg4rang@naver.com>
2024-09-10 14:39:08 +0900

    [refactor] posting detial provider가 coordi repository 사용할 수 있도록 수정

--- lyc_flutter_project/lib/posting/provider/posting_detail_provider.dart
+++ lyc_flutter_project/lib/posting/provider/posting_detail_provider.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:lyc_flutter_project/mypage/provider/mypage_provider.dart';
 import 'package:lyc_flutter_project/posting/model/coordi_posting.dart';
+import 'package:lyc_flutter_project/posting/repository/coordi_repository.dart';
 
 class PostingDetailProviderFactory extends ChangeNotifier {
   final MypageProvider mypageProvider;
+  final CoordiRepositoryProvider coordiRepositoryProvider;
   final Map<int, PostingDetailProvider> _providers = {};
 
-  PostingDetailProviderFactory({required this.mypageProvider});
+  PostingDetailProviderFactory({
+    required this.mypageProvider,
+    required this.coordiRepositoryProvider,
+  });
 
   PostingDetailProvider getProvider(int postingId) {
     if (!_providers.containsKey(postingId)) {
       _providers[postingId] = PostingDetailProvider(
         mypageProvider: mypageProvider,
+        coordiRepositoryProvider: coordiRepositoryProvider,
         postingId: postingId,
       );
     }
@@ -35,10 +41,12 @@ class PostingDetailProviderFactory extends ChangeNotifier {
 
 class PostingDetailProvider extends ChangeNotifier {
   final MypageProvider mypageProvider;
+  final CoordiRepositoryProvider coordiRepositoryProvider;
   final int postingId;
 
   PostingDetailProvider({
     required this.mypageProvider,
+    required this.coordiRepositoryProvider,
     required this.postingId,
   });
 
