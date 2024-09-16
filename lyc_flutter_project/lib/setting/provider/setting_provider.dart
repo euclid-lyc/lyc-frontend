8b02c9e650c29e1b89b214a93f451a5952949c97
doyeon0307 <eg4rang@naver.com>
2024-09-16 21:23:20 +0900

    [feat] add setting provider

---
+++ lyc_flutter_project/lib/setting/provider/setting_provider.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/foundation.dart';
+import 'package:lyc_flutter_project/setting/repository/setting_repository.dart';
+
+class SettingProvider extends ChangeNotifier {
+  final SettingRepositoryProvider repositoryProvider;
+
+  SettingProvider({
+    required this.repositoryProvider,
+  });
+}
