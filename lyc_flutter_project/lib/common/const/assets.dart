026e77193372e1e557050557b2dbbb5f279e082d
doyeon0307 <eg4rang@naver.com>
2024-12-23 21:10:10 +0900

    [add] assets

---
+++ lyc_flutter_project/lib/common/const/assets.dart
@@ -0,0 +1,6 @@
+class Assets {
+  static const String cameraButton = 'assets/button/camera.svg';
+  static const String commissionButton = 'assets/button/commission.svg';
+  static const String photoButton = 'assets/button/photo.svg';
+  static const String scheduleButton = 'assets/button/schedule.svg';
+}
