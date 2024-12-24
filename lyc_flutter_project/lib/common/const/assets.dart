07230e9d521acb7885bef774f3377b8d00c15666
doyeon0307 <eg4rang@naver.com>
2024-12-24 14:57:26 +0900

    [add] out icon

--- lyc_flutter_project/lib/common/const/assets.dart
+++ lyc_flutter_project/lib/common/const/assets.dart
@@ -1,6 +1,7 @@
 class Assets {
   // icons
   static const String menu = 'assets/icon/menu.svg';
+  static const String out = 'assets/icon/out.svg';
 
   // buttons
   static const String cameraButton = 'assets/button/camera.svg';
