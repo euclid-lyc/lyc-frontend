59e12dc659ceddcccb9c22eeb8270fa515536330
doyeon0307 <eg4rang@naver.com>
2025-01-03 22:26:32 +0900

    [add] typo

---
+++ lyc_flutter_project/lib/common/const/typographies.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class Typos {
+  const Typos._();
+
+  static const TextStyle regular18 = TextStyle(
+    fontSize: 18.0,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle semibold16 = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle regular16 = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle regular14 = TextStyle(
+    fontSize: 14.0,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle regular12 = TextStyle(
+    fontSize: 12.0,
+    fontWeight: FontWeight.w400,
+  );
+}
