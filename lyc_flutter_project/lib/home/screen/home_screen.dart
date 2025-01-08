b87f665cca398e6cfc0b4fb18fd5103d875c2e09
s1hwa <142314061+s1hwa@users.noreply.github.com>
2025-01-09 08:20:19 +0900

    오류 수정

--- lyc_flutter_project/lib/home/screen/home_screen.dart
+++ lyc_flutter_project/lib/home/screen/home_screen.dart
@@ -11,8 +11,6 @@ import 'package:lyc_flutter_project/mypage/model/mypage_posting_preview.dart';
 import 'package:lyc_flutter_project/routes/routes.dart';
 import 'package:provider/provider.dart';
 
-import '../../commissions/screens/primary_info_screen.dart';
-
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -182,19 +180,8 @@ class _HomeScreenState extends State<HomeScreen> {
                               const SizedBox(height: 30),
                               Row(
                                 children: [
-                                  GestureDetector(
-                                    onTap: () {
-                                      Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (context) => const PrimaryInfoScreen(),
-                                        ),
-                                      );
-                                    },
-                                    child: SvgPicture.asset(
-                                      'assets/icon_facebook.svg',
-                                    ),
-                                  ),
+                                  SvgPicture.asset('assets/icon_facebook.svg'),
+                                  const SizedBox(width: 10),
                                   SvgPicture.asset('assets/icon_linkedin.svg'),
                                   const SizedBox(width: 10),
                                   SvgPicture.asset('assets/icon_twitter.svg'),
